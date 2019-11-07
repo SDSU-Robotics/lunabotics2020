196e2112c72e1b71234f90190bae9730aea28245
Joel Quanbeck <jqquanbeck@gmail.com>
2019-11-07 14:22:54 -0600

    Added lidar data callback function

--- src/Localization.cpp
+++ src/Localization.cpp
@@ -3,8 +3,24 @@
 
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
+#include "sensor_msgs/LaserScan.h"
 
 using namespace std;
+
+class Listener
+{
+public:
+	void scanCB(const sensor_msgs::LaserScan msg);
+
+private:
+	
+};
+
+void Listener::scanCB(const sensor_msgs::LaserScan msg)
+{
+	cout << msg.angle_min << endl;
+	cout << "Msg received" << endl;
+}
 
 int main (int argc, char **argv)
 {
@@ -12,9 +28,9 @@ int main (int argc, char **argv)
 	ros::NodeHandle n;
 	ros::Rate loop_rate(10);
 
-	//Listener listener;
+	Listener listener;
 
-	//ros::Subscriber joySub = n.subscribe("joy", 100, &Listener::joyListener, &listener);
+	ros::Subscriber scan_sub = n.subscribe("scan", 100, &Listener::scanCB, &listener);
 	//ros::Subscriber rpm_sub = n.subscribe("calculated_rpm", 100, &Listener::rpmListener, &listener);
 
 	ros::Publisher x_pub = n.advertise<std_msgs::Float64>("x", 1000);
