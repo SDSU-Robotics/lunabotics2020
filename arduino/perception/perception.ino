738da8f536e9bfb321f0df9cd1f218c63491cfa5
Joel Quanbeck <jqquanbeck@gmail.com>
2020-02-09 16:27:32 -0600

    Added ROS functionality

--- arduino/perception/perception.ino
+++ arduino/perception/perception.ino
@@ -1,5 +1,5 @@
 /*
- moves motor according to input in degrees
+ sweeps motor between 2 degree values
 
  This program drives a unipolar or bipolar stepper motor.
  The motor is attached to digital pins 8 - 11 of the Arduino.
@@ -16,52 +16,94 @@
  Jack Winter
 
  */
-
+#include <ros.h>
+#include <std_msgs/Float32.h>
 #include <Stepper.h>
 
-const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+//ROS Stuff
+ros::NodeHandle nh;
+std_msgs::Float32 angle_msg;
+ros::Publisher angle_pub("lidar_angle", &angle_msg);
+
+const int stepsPerRevolution = 513;  // change this to fit the number of steps per revolution
 // for your motor
 
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-int stepCount = 0;         // number of steps the motor has taken
+int currentStep = 0;         // number of steps the motor has taken
+const int STEPINC = 3;
+const float GEAR_RATIO = 31.0/10.0;
 
-void setup() {
+float lidarAngle(float,float,float);
+int degToStep(float);//converts a degree(float) to a step(int)
+float stepToDeg(int);//convert a step(int) to a degree(float)
+int gotToStep(int); //input desired position in steps //currentStep position
+
+
+const int upperPos = degToStep( 0);
+const int lowerPos = degToStep( -45);
+
+void setup() 
+{ 
+  //ROS Stuff
+  nh.initNode();
+  nh.advertise(angle_pub);  
+  
   // set the speed at 60 rpm:
   myStepper.setSpeed(60);
   // initialize the serial port:
-  Serial.begin(9600);
+  //Serial.begin(9600);
+
+  //get lidar to 0 degrees
+
+  //move until button press
+
+  //while(!buttonPress)
+  //{
+  //  myStepper.step(-1);
+  //}
+  //currentStep = 0;
 }
 
-float rx_float;
-int   rx_int;
-
-
 void loop() {
-//input degrees
-     if (Serial.available() > 0) {    // is a character available?
-    rx_float = Serial.parseFloat();       // get the character
-    Serial.println(rx_float);
-    
-   
-//convert from 360* to 2048 steps per revolution
-    rx_float = rx_float * stepsPerRevolution/360;
-    rx_int = (int) rx_float;
-    Serial.println(rx_int);
-//move motor
-   
-    myStepper.step(rx_int);
-  
-  // step one step:
-  /*myStepper.step(1);
-  Serial.print("steps:");
-  Serial.println(stepCount);
-  stepCount++;
-  delay(500);
-  */
+
+for(int i = lowerPos; i<=upperPos; i+=STEPINC) //move clockwise
+  {
+     goToStep(i);
+     angle_msg.data = stepToDeg(currentStep);
+     angle_pub.publish(&angle_msg);
+     nh.spinOnce();
+     delay(10);
+  }  
+  for(int i = upperPos; i>=lowerPos; i-=STEPINC) //move counter-clockwise
+  {
+     goToStep(i);
+     angle_msg.data = stepToDeg(currentStep);
+     angle_pub.publish(&angle_msg);
+     nh.spinOnce();
+     delay(10);
   }
-  else{
-    Serial.flush();
-  }
+}
+
+int goToStep(int nextStep)
+{
+  myStepper.step(nextStep - currentStep);
+  currentStep = nextStep;
+  return (currentStep);
+}
+
+float stepToDeg(int steps)
+{
+  float degreeOut = (steps * 360.0) / ((float) stepsPerRevolution * 4 * GEAR_RATIO);
+
+  return degreeOut;
+}
+
+int degToStep(float degreesIn)
+{
+  //convert degree of interest into steps 
+   int stepsOut = (degreesIn / 360.0) * (float)stepsPerRevolution *4.0 * GEAR_RATIO;
+
+    return stepsOut;
 }
