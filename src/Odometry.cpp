f89c1c25e8b1917ca4de5635cbf479a19dd3008f
Rick Kittelson <rakittelson721@gmail.com>
2020-03-07 15:17:23 -0600

    Update Odometry.cpp

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -19,7 +19,7 @@
 
 int main (int argc, char **argv)
 {
-    ros::init(argc, argv, "Odometry");
+	ros::init(argc, argv, "Odometry");
 	ros::NodeHandle n;
 	ros::Rate loop_rate(100);
 
@@ -35,30 +35,30 @@ int main (int argc, char **argv)
 
 	while (ros::ok())
 	{
-        //since all odometry is 6DOF we'll need a quaternion created from yaw
-        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(vth);
+		//since all odometry is 6DOF we'll need a quaternion created from yaw
+		geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(vth);
 
-        //next, we'll publish the odometry message over ROS
-        nav_msgs::Odometry odom;
-        odom.header.stamp = ros::Time::now();
-        odom.header.frame_id = "transportation"; //Center frame of odometry message
+		//next, we'll publish the odometry message over ROS
+		nav_msgs::Odometry odom;
+		odom.header.stamp = ros::Time::now();
+		odom.header.frame_id = "transportation"; //Center frame of odometry message
 
-        //set the position
-        odom.pose.pose.position.x = 0.0;
-        odom.pose.pose.position.y = 0.0;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = odom_quat;
+		//set the position
+		odom.pose.pose.position.x = 0.0;
+		odom.pose.pose.position.y = 0.0;
+		odom.pose.pose.position.z = 0.0;
+		odom.pose.pose.orientation = odom_quat;
 
-        //set the velocity
-        odom.child_frame_id = "map";
-        odom.twist.twist.linear.x = vx;
-        odom.twist.twist.linear.y = vy;
-        odom.twist.twist.angular.z = vth;
+		//set the velocity
+		odom.child_frame_id = "map";
+		odom.twist.twist.linear.x = vx;
+		odom.twist.twist.linear.y = vy;
+		odom.twist.twist.angular.z = vth;
 
-        //publish the message
-        odom_pub.publish(odom);
-	
-        vth += .01;
+		//publish the message
+		odom_pub.publish(odom);
+
+		vth += .01;
 
 		ros::spinOnce();
 		loop_rate.sleep();
