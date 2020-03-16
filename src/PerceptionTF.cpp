a97bc33001413a540a665d5b10ce3185968dae80
Joel Quanbeck <jqquanbeck@gmail.com>
2020-03-16 14:28:44 -0500

    Cleanup

--- src/PerceptionTF.cpp
+++ src/PerceptionTF.cpp
@@ -9,31 +9,30 @@
 #include <math.h>
 
 #define PI 3.141592653589
+
+#define SLS_HEIGHT 0.098 // meters
+#define LEVER_LENGTH 0.005 // meters
+
 using namespace std;
 
-const float leverLength = 0.005; // meters
-
 void rpy_cb(const geometry_msgs::Vector3Stamped rpy_in){
+	
 	static tf2_ros::TransformBroadcaster br;
 	geometry_msgs::TransformStamped transformStamped;
 
-	float pitch =  rpy_in.vector.y+PI/2.0; // radians
-	//cout<<pitch<<endl;
+	float pitch =  rpy_in.vector.y + PI / 2.0; // radians
 
 	transformStamped.header.stamp = ros::Time::now();
 	transformStamped.header.frame_id = "map";
 	transformStamped.child_frame_id = "front_lidar_frame";
 	transformStamped.transform.translation.x = 0.0;
-	transformStamped.transform.translation.y = leverLength * cos(pitch);
-	transformStamped.transform.translation.z = .58 + leverLength * sin(pitch);
+	transformStamped.transform.translation.y = LEVER_LENGTH * cos(pitch);
+	transformStamped.transform.translation.z = SLS_HEIGHT + LEVER_LENGTH * sin(pitch);
 
 	tf2::Quaternion q;
-	q.setRPY(0.0, -1*pitch, 0.0);
+	q.setRPY(0.0, -1 * pitch, 0.0);
 
-	transformStamped.transform.rotation.x = q.x();
-	transformStamped.transform.rotation.y = q.y();
-	transformStamped.transform.rotation.z = q.z();
-	transformStamped.transform.rotation.w = q.w();
+	transformStamped.transform.rotation = q;
 
 	br.sendTransform(transformStamped);
 }
@@ -41,8 +40,11 @@ void rpy_cb(const geometry_msgs::Vector3Stamped rpy_in){
 int main(int argc, char** argv){
 	ros::init(argc, argv, "PerceptionTF");
 	
-	ros::NodeHandle nh;
-	ros::Subscriber rpy_sub = nh.subscribe("/imu/rpy",10,&rpy_cb);
+	ros::NodeHandle n;
+
+	ros::Subscriber rpy_sub = n.subscribe("/imu/rpy", 10, &rpy_cb);
+
 	ros::spin();
+
 	return 0;
 };
