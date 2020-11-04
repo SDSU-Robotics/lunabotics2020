a3b59ad1c300d5266e44781e5b4c87ca8246c4ac
Ryan Fick <ryan.fick@jacks.sdstate.edu>
2020-11-04 17:35:42 -0600

    added Tport Drive base

--- src/DeviceIDs.h
+++ src/DeviceIDs.h
@@ -6,11 +6,14 @@ namespace DeviceIDs
 {
 	static const int ExcvDrvLTal = 1;
 	static const int ExcvDrvRTal = 2;
-	
 	static const int ExcvExtendVic = 1;
 	static const int ExcvPitchVic = 2;
 	static const int ExcvDriveVic = 3;
 	static const int ExcvConveyorDrvVic = 4;
+
+	static const int TPortConveyorDrvVic = 5;
+	static const int TportDrvLTal = 6;
+	static const int TportDrvRTal = 7;
 
 };
 
