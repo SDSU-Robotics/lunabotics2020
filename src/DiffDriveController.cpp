ebe0458e5b2827225063ccc9798148769cff84b9
sarahsd0109 <sarah.aman@jacks.sdstate.edu>
2020-11-10 18:37:05 -0500

    added comments & created excavator toggle

--- src/DiffDriveController.cpp
+++ src/DiffDriveController.cpp
@@ -1,3 +1,5 @@
+//This program gets values from a joystick controller publishes them &
+// toggles each button on and off after button press
 #include <string>
 #include <unistd.h>
 #include <iostream>
@@ -17,12 +19,14 @@ public:
 
 
 private:
-    bool _buttons[12] = { 0 };
-	double _axes[6] = { 0 };
+    bool _buttons[12] = { 0 }; // declare array for button values
+	double _axes[6] = { 0 }; // declare array for axes values
 };
 
 
-void Listener::joyListener(const sensor_msgs::Joy::ConstPtr& Joy)
+void Listener::joyListener(const sensor_msgs::Joy::ConstPtr& Joy) 
+// listens for the array value of buttons and axes from sensor messages
+// & sets them to false
 {
 	for (int i = 0 ; i < 12; i++)
 		_buttons[i] = Joy->buttons[i];
@@ -32,6 +36,7 @@ void Listener::joyListener(const sensor_msgs::Joy::ConstPtr& Joy)
 }
 
 void Listener::getJoyVals(bool buttons[], double axes[]) const
+// gets the values from the buttons and axes arrays and sets them to either true or falso
 {
     for (int i = 0; i < 12; i++)
         buttons[i] = _buttons[i];
@@ -85,15 +90,18 @@ int main (int argc, char **argv)
 	ros::Publisher l_speed_pub = n.advertise<std_msgs::Float32>("ExcvLDrvPwr", 100);
     ros::Publisher r_speed_pub = n.advertise<std_msgs::Float32>("ExcvRDrvPwr", 100);
 	ros::Publisher conveyor_pwr_pub = n.advertise<std_msgs::Float32>("ExcvConveyorDrvPwr", 100);
+	ros::Publisher excavator_pwr_pub = n.advertise<std_msgs::Float32>("ExcvTrencherDrvPwr", 100);
 
     std_msgs::Float32 l_speed_msg;
     std_msgs::Float32 r_speed_msg;
 	std_msgs::Float32 conveyor_pwr_msg;
+	std_msgs::Float32 excavator_pwr_pub;
 	
 	while (ros::ok())
 	{
         listener.getJoyVals(buttons, axes);
 		listener.toggle(buttons[0], currentButton, on, conveyor_pwr_msg);
+		listener.toggle(buttons[1], currentButton, on, excavator_pwr_msg);
 
 		l_speed_msg.data = axes[1]; // left Y
 		r_speed_msg.data = axes[3]; // right Y
@@ -101,6 +109,7 @@ int main (int argc, char **argv)
 		l_speed_pub.publish(l_speed_msg);
 		r_speed_pub.publish(r_speed_msg);
 		conveyor_pwr_pub.publish(conveyor_pwr_msg);
+		excavator_pwr_pub.publish(excavator_pwr_msg);
 		
 		ros::spinOnce();
 		loop_rate.sleep();
