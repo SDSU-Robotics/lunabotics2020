38e3e6436f45367583cf862785dc394c37eedfbf
sarahsd0109 <sarah.aman@jacks.sdstate.edu>
2020-11-16 19:55:16 -0500

    Joystick for Trencher

--- src/DiffDriveController.cpp
+++ src/DiffDriveController.cpp
@@ -45,6 +45,63 @@ void Listener::getJoyVals(bool buttons[], double axes[]) const
         axes[i] = _axes[i];
 }
 
+
+int main (int argc, char **argv)
+{
+    ros::init(argc, argv, "DriveController");
+	ros::NodeHandle n;
+	ros::Rate loop_rate(100);
+
+	Listener listener;
+
+	ros::Subscriber joySub = n.subscribe("joy", 100, &Listener::joyListener, &listener);
+	
+	bool buttons[12];
+	double axes[6];
+	// currentButton and on will need to be seperate booleans for each array value. 
+	bool currentButton4 = 0;
+	bool on1 = false;
+	bool currentButton5 = 0;
+	bool on0 = false;
+	double stepSize = 1;
+	double speed = 0;
+	double maxSpeed = 10;
+
+
+
+	// Publishes the message to the hardware interface
+	ros::Publisher l_speed_pub = n.advertise<std_msgs::Float32>("ExcvLDrvPwr", 100);
+    ros::Publisher r_speed_pub = n.advertise<std_msgs::Float32>("ExcvRDrvPwr", 100);
+	ros::Publisher conveyor_pwr_pub = n.advertise<std_msgs::Float32>("ExcvConveyorDrvPwr", 100);
+	ros::Publisher excavator_pwr_pub = n.advertise<std_msgs::Float32>("driveSpeedSub", 100);
+
+	// sets the message to the message variable
+    std_msgs::Float32 l_speed_msg;
+    std_msgs::Float32 r_speed_msg;
+	std_msgs::Float32 conveyor_pwr_msg;
+	std_msgs::Float32 excavator_pwr_msg;
+	
+	while (ros::ok()) // runs while ros is running
+	{
+        listener.getJoyVals(buttons, axes);
+		listener.toggleDrvSpeedUp(buttons[5], currentButton5, maxSpeed, speed, stepSize, &excavator_pwr_msg, );
+		listener.toggleDrvSpeedDown(buttons[4], currentButton4, maxSpeed, speed, stepSize, &excavator_pwr_msg);
+
+		l_speed_msg.data = axes[1]; // left Y
+		r_speed_msg.data = axes[3]; // right Y
+		
+		l_speed_pub.publish(l_speed_msg); // left speed
+		r_speed_pub.publish(r_speed_msg); // right speed
+		conveyor_pwr_pub.publish(conveyor_pwr_msg); // conveyor power
+		excavator_pwr_pub.publish(excavator_pwr_msg); // excavator power
+		
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
+
 void Listener::toggleDrvSpeedUp(const bool keys, bool &currentButton, double maxSpeed, double speed, double stepSize, std_msgs::Float32 &message)
 {
 
@@ -119,60 +176,5 @@ void Listener::toggleDrvSpeedDown(const bool keys, bool &currentButton, double m
 		//Toggle On button release
 	}
 
-int main (int argc, char **argv)
-{
-    ros::init(argc, argv, "DriveController");
-	ros::NodeHandle n;
-	ros::Rate loop_rate(100);
-
-	Listener listener;
-
-	ros::Subscriber joySub = n.subscribe("joy", 100, &Listener::joyListener, &listener);
-	
-	bool buttons[12];
-	double axes[6];
-	// currentButton and on will need to be seperate booleans for each array value. 
-	bool currentButton4 = 0;
-	bool on1 = false;
-	bool currentButton5 = 0;
-	bool on0 = false;
-	double stepSize = 1;
-	double speed = 0;
-	double maxSpeed = 10;
-
-
-
-	// Publishes the message to the hardware interface
-	ros::Publisher l_speed_pub = n.advertise<std_msgs::Float32>("ExcvLDrvPwr", 100);
-    ros::Publisher r_speed_pub = n.advertise<std_msgs::Float32>("ExcvRDrvPwr", 100);
-	ros::Publisher conveyor_pwr_pub = n.advertise<std_msgs::Float32>("ExcvConveyorDrvPwr", 100);
-	ros::Publisher excavator_pwr_pub = n.advertise<std_msgs::Float32>("driveSpeedSub", 100);
-
-	// sets the message to the message variable
-    std_msgs::Float32 l_speed_msg;
-    std_msgs::Float32 r_speed_msg;
-	std_msgs::Float32 conveyor_pwr_msg;
-	std_msgs::Float32 excavator_pwr_msg;
-	
-	while (ros::ok()) // runs while ros is running
-	{
-        listener.getJoyVals(buttons, axes);
-		listener.toggleDrvSpeedUp(buttons[5], currentButton5, maxSpeed, speed, stepSize, &excavator_pwr_msg, );
-		listener.toggleDrvSpeedDown(buttons[4], currentButton4, maxSpeed, speed, stepSize, &excavator_pwr_msg);
-
-		l_speed_msg.data = axes[1]; // left Y
-		r_speed_msg.data = axes[3]; // right Y
-		
-		l_speed_pub.publish(l_speed_msg); // left speed
-		r_speed_pub.publish(r_speed_msg); // right speed
-		conveyor_pwr_pub.publish(conveyor_pwr_msg); // conveyor power
-		excavator_pwr_pub.publish(excavator_pwr_msg); // excavator power
-		
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
-
-	return 0;
-}
 
 
