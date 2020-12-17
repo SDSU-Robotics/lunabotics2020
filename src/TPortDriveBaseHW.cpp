41d2ecd66e686e5dd0895b10d26cada9181c8594
JoelByers <joel.byers@outlook.com>
2020-12-17 14:22:10 -0600

    Twist Translator

--- src/TPortDriveBaseHW.cpp
+++ src/TPortDriveBaseHW.cpp
@@ -1,15 +1,20 @@
 #include "ctre/Phoenix.h"
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
+#include "geometry_msgs/Twist.h"
 #include "ctre/phoenix/platform/Platform.h"
 #include "ctre/phoenix/unmanaged/Unmanaged.h"
 #include "DeviceIDs.h"
+#include <iostream>
 
 using namespace std;
 using namespace ctre::phoenix;
 using namespace ctre::phoenix::platform;
 using namespace ctre::phoenix::motorcontrol;
 using namespace ctre::phoenix::motorcontrol::can;
+
+#define LINEAR_ADJ 1
+#define ANGULAR_ADJ 1
 
 /****************************************************************************
 ****     This node subscribes to the motor values set in TportRDrvPwr and****
@@ -27,6 +32,7 @@ class Listener
 
         void setLSpeed(const std_msgs::Float32 lspeed);
         void setRSpeed(const std_msgs::Float32 rspeed);
+		void setTwistSpeed(const geometry_msgs::Twist twist);
 
 
    //private:
@@ -46,6 +52,7 @@ int main (int argc, char **argv)
 
 	ros::Subscriber lSpeedSub = n.subscribe("TPortLDrvPwr", 100, &Listener::setLSpeed, &listener);
 	ros::Subscriber rSpeedSub = n.subscribe("TPortRDrvPwr", 100, &Listener::setRSpeed, &listener);
+	ros::Subscriber twistSpeedSub = n.subscribe("cmd_vel", 100, &Listener::setTwistSpeed, &listener);
 
 	while (ros::ok())
 	{
@@ -73,4 +80,12 @@ void Listener::setRSpeed(const std_msgs::Float32 rspeed)
     rightDrive.Set(ControlMode::PercentOutput, rspeed.data);
 
 	ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
+}
+
+void Listener::setTwistSpeed(const geometry_msgs::Twist twist)
+{
+	leftDrive.Set(ControlMode::PercentOutput, LINEAR_ADJ * twist.linear.x + ANGULAR_ADJ * twist.angular.z)
+	rightDrive.Set(ControlMode::PercentOutput, LINEAR_ADJ * twist.linear.x - ANGULAR_ADJ * twist.angular.z)
+
+	ctre::phoenix::unmanaged::FeedEnable(100);
 }
