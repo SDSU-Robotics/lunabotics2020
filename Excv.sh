da58793cfb71215d5690eedabd931dbc0f1d3e3f
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-02-03 19:01:41 -0600

    Excv.sh, Rosh.sh. TPort.sh

--- Excv.sh
+++ Excv.sh
@@ -1,3 +1,4 @@
+roscore
 export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
 #export ROS_HOSTNAME=robotics-Latitude-E6420:11311
-roslaunch lunabotics2020 TPort.launch;
+roslaunch lunabotics2020 Excv.launch;
--- ROS.sh
+++ ROS.sh
@@ -1,6 +1,6 @@
 
+roscore
 export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
-#export ROS_HOSTNAME=robotics-Latitude-E6420:11311
 roslaunch lunabotics2020 Joy.launch;
-
+#rosrun rospy_tutorials talker.py
 
