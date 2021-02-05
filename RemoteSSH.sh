5053ec078866a101a916c6d4d844a880e795d4e9
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-02-04 21:02:06 -0600

    RemoterSSH.sh

--- RemoteSSH.sh
+++ RemoteSSH.sh
@@ -1,2 +1,2 @@
-gnome-terminal -- /bin/sh -c 'ssh 192.168.1.102; exec bash';    #udu
-gnome-terminal -- /bin/sh -c 'ssh 192.168.1.101; exec bash';    #pi
+gnome-terminal -- /bin/sh -c 'echo UDU;ssh 192.168.1.102; exec bash';    #udu
+gnome-terminal -- /bin/sh -c 'echo PI;ssh 192.168.1.101; exec bash';    #pi
