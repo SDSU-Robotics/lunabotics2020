4f445aacb515603f4c2d3e7b05330488d899a1c5
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-02-06 16:20:46 -0600

    ROS.sh, RemoteSSH.sh

--- ROS.sh
+++ ROS.sh
@@ -1,5 +1,6 @@
 
 export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
+export ROS_IP=172.31.99.168
 roslaunch lunabotics2020 Joy.launch
 
 
--- RemoteSSH.sh
+++ RemoteSSH.sh
@@ -1,2 +1,2 @@
-gnome-terminal -- /bin/sh -c 'echo UDU;ssh 192.168.1.102; exec bash';    #udu
-gnome-terminal -- /bin/sh -c 'echo PI;ssh 192.168.1.101; exec bash';    #pi
+gnome-terminal -- /bin/sh -c 'echo UDU;ssh 172.31.99.109; exec bash';    #udu
+gnome-terminal -- /bin/sh -c 'echo PI;ssh 172.31.99.76; exec bash';    #pi
