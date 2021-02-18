920fcec5941ef51681735a34ecfc271d32129910
Ryan Fick <ryan.fick@jacks.sdstate.edu>
2021-02-17 19:03:54 -0600

    thing

--- ROS.sh
+++ ROS.sh
@@ -1,5 +1,5 @@
 
-export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
+export ROS_MASTER_URI=http://robotics-Latitude-E6410:11311
 export ROS_IP=172.31.99.109
 roslaunch lunabotics2020 Joy.launch
 
