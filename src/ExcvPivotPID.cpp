cb5d70eb127bb10353c7031e88cdc0c31eb7c133
Rick Kittelson <rakittelson721@gmail.com>
2021-03-29 19:55:04 -0500

    New Pivot PID Code/Node

---
+++ src/ExcvPivotPID.cpp
@@ -0,0 +1,157 @@
+#include "ctre/Phoenix.h"
+#include "ctre/phoenix/platform/Platform.h"
+#include "ctre/phoenix/unmanaged/Unmanaged.h"
+#include "DeviceIDs.h"
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+
+using namespace std;
+using namespace ctre::phoenix;
+using namespace ctre::phoenix::platform;
+using namespace ctre::phoenix::motorcontrol;
+using namespace ctre::phoenix::motorcontrol::can;
+
+/***************************************************************************
+****     This node sets the motor position of a linear actuator using   ****
+****         the TalonSRX PID capabilities                              ****
+****     Subscribers:                                                   ****
+****          std_msgs/Float32 ExcvTrencherPos - trencher set position  ****
+****     Publishers:                                                    ****
+****          std_msgs/Float32 actualPosition - trencher actual position****
+****          std_msgs/Float32 controlEffort - control effort applied   ****
+***************************************************************************/
+
+//Subscribe to the actualPosition topic and manually extend and retract the linear actuator
+//		set these values equal to the max and min potentiometer values recorded 
+//		*(numbers are flipped so that 1.0 is full extension and 0.0 is full retraction)
+
+#define MIN_QUAD_READING -2300
+#define MAX_QUAD_READING 2300
+
+//Minimum and maximum input values for the actuator position EX: value sent from controller.
+//		These will map to the above values respectively
+#define MIN_LINEAR_INPUT 0.0
+#define MAX_LINEAR_INPUT 1.0
+
+
+class Listener
+{
+public:
+	Listener();
+	void setPosition(const std_msgs::Float32 msg);
+	double getActualPosition();
+	double getPercentOutput();
+	TalonSRX _motor = {DeviceIDs::ExcvPitchTal};
+};
+
+
+int main (int argc, char **argv)
+{
+	ros::init(argc, argv, "ExcvPivotPID");
+	ros::NodeHandle n;
+	ros::Rate loop_rate(100);
+	
+	ctre::phoenix::platform::can::SetCANInterface("can0");
+
+	ros::Publisher extend_current_pub = n.advertise<std_msgs::Float32>("ExcvPivotCurrent", 100);
+
+	std_msgs::Float32 extend_current_msg;
+
+	Listener listener;
+
+	ros::Subscriber position_sub = n.subscribe("ExcvPivotPos", 1000, &Listener::setPosition, &listener);
+
+	ros::Publisher actualPosition_pub = n.advertise<std_msgs::Float32>("actualPositionPivot", 100);
+	ros::Publisher controlEffort_pub = n.advertise<std_msgs::Float32>("controlEffortPivot", 100);
+
+	std_msgs::Float32 actualPosition_msg;
+	std_msgs::Float32 controlEffort_msg;
+
+	while (ros::ok())
+	{
+		actualPosition_msg.data = listener.getActualPosition();
+		actualPosition_pub.publish(actualPosition_msg);
+		
+		controlEffort_msg.data = listener.getPercentOutput();
+		controlEffort_pub.publish(controlEffort_msg);
+
+		extend_current_msg.data = listener._motor.GetOutputCurrent();
+		extend_current_pub.publish(extend_current_msg);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
+
+
+void Listener::setPosition(const std_msgs::Float32 msg)
+{
+	// limit values
+	float pos = msg.data;
+
+	//Safeguarding addressing values that are larger or smaller than the defined input min/max
+	if (pos < MIN_LINEAR_INPUT)	pos = MIN_LINEAR_INPUT;
+	if (pos > MAX_LINEAR_INPUT)	pos = MAX_LINEAR_INPUT;
+
+	//Linearly maps pos between MIN_POT_READING and MAX_POT_READING
+	pos = LinearInterpolation::Calculate(pos, MIN_LINEAR_INPUT, MIN_QUAD_READING, MAX_LINEAR_INPUT, MAX_QUAD_READING);
+
+	//Set motor to newly mapped position
+	_motor.Set(ControlMode::Position, pos);
+
+	ctre::phoenix::unmanaged::FeedEnable(100); // feed watchdog
+}
+
+
+Listener::Listener()
+{
+	TalonSRXConfiguration motorProfile;
+
+	ros::Duration(1.0).sleep();    // give time to finish initialization before trying to read position data, otherwise it will fail
+
+	//Threshold for zero-motion for the neutral position.
+	motorProfile.neutralDeadband = 0.01;
+			
+	//Peak Speed Config
+	motorProfile.peakOutputForward = 1.0;
+	motorProfile.peakOutputReverse = -1.0;
+	
+	//Ramp Config
+	motorProfile.closedloopRamp = 1.5f;
+			
+	//PID Config - These will determine how the the PID breakout board is wired, lookup a pinout and these will match the pins
+	//motorProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::QuadEncoder;
+	motorProfile.primaryPID.selectedFeedbackSensor = FeedbackDevice::QuadEncoder;
+
+	motorProfile.primaryPID.selectedFeedbackCoefficient = 1.0f;//0.25f;// 0.328293f;
+
+	//PID Constants
+	motorProfile.slot0.kP                       = 10.0f; //0.01f; //Propotional Constant.  Controls the speed of error correction.
+	motorProfile.slot0.kI                       = 0.01f; //Integral Constant.     Controls the steady-state error correction.
+	motorProfile.slot0.kD                       = 0.0f; //Derivative Constant.   Controls error oscillation.
+	motorProfile.slot0.kF                       = 0.0f; //Feed Forward Constant. (IDK what this does)
+	motorProfile.slot0.integralZone             = 100000;   //Maximum value for the integral error accumulator. Automatically cleared when exceeded.
+	motorProfile.slot0.maxIntegralAccumulator   = 10000;   //Maximum value for the integral error accumulator. (IDK what this does)
+	motorProfile.slot0.allowableClosedloopError = 0.001;   //If the total error-value is less than this value, the error is automatically set to zero.
+	motorProfile.slot0.closedLoopPeakOutput     = 1.0f; //Peak output for the PID Controller.
+	motorProfile.slot0.closedLoopPeriod         = 100;   //Samples per second (?) (IDK what this is)
+
+	_motor.ConfigAllSettings(motorProfile);
+
+	_motor.SetNeutralMode(NeutralMode::Brake);
+	_motor.SetInverted(false);
+	_motor.SetSensorPhase(true);
+}
+
+// reads speed of motor shaft and returns in meters/sec
+double Listener::getActualPosition()
+{
+	return _motor.GetSelectedSensorPosition();
+}
+
+double Listener::getPercentOutput()
+{
+	return _motor.GetClosedLoopError();
+}
