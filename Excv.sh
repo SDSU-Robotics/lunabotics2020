80da789fc159128f293ea08f17abbb4ad70197ff
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-04-24 14:13:09 -0500

    changes rosmasteruri

--- Excv.sh
+++ Excv.sh
@@ -1,3 +1,3 @@
 
-export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
+export ROS_MASTER_URI=http://192.168.0.128:11311
 roslaunch lunabotics2020 Excv.launch
--- src/ExcvController.cpp
+++ src/ExcvController.cpp
@@ -229,7 +229,7 @@ void Listener::toggleDoor (const bool keys, bool &currentButton, bool &on, std_m
 	if (on)
 	{
 		ROS_INFO("Button on");
-		message.data = 90;
+		message.data = 145;
 
 	}
 	else
