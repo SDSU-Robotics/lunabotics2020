a7851c4822034dec2039376755f77d22d1cf6c79
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-04-24 14:38:42 -0500

    changes made to ROSIP in Excv

--- ROS.sh
+++ ROS.sh
@@ -1,3 +1,3 @@
-export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
+export ROS_MASTER_URI=http://192.168.0.128:11311
 export ROS_IP=192.168.0.128 #172.31.99.168 - rp wifi
 roslaunch lunabotics2020 Joy.launch
