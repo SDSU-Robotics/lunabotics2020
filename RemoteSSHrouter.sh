961b2bbbc08ae31f50d4a14cbc98518eb06fedd4
Bhagya-SDSU <SVDBhagyaKalhara.Samaranayake@jacks.sdstate.edu>
2021-04-29 19:07:03 -0500

    made ssh file for both router and wifi

---
+++ RemoteSSHrouter.sh
@@ -0,0 +1,2 @@
+gnome-terminal -- /bin/sh -c 'echo Dug UDOO;ssh robotics@192.168.0.103; exec bash';    #dug udoo
+gnome-terminal -- /bin/sh -c 'echo Dig UDOO;ssh robotics@192.168.0.132; exec bash';    #dig udoo
---
+++ RemoteSSHwifi.sh
@@ -0,0 +1,2 @@
+gnome-terminal -- /bin/sh -c 'echo Dug UDOO;ssh robotics@172.31.99.23; exec bash';    #dug udoo
+gnome-terminal -- /bin/sh -c 'echo Dig UDOO;ssh robotics@172.31.99.35; exec bash';    #dig udoo
