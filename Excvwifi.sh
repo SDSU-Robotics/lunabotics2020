97cf47a116b2b4b0c2420e785c8705b27352d9eb
Ryan Fick <ryan.fick@jacks.sdstate.edu>
2021-05-04 18:08:49 -0500

    wireless

--- Excvwifi.sh
+++ Excvwifi.sh
@@ -1,2 +1,2 @@
-export ROS_MASTER_URI=http://robotics-Latitude-E6420:11311
+export ROS_MASTER_URI=http://robotics-Latitude-E6410:11311
 roslaunch lunabotics2020 Excv.launch
--- ROSwifi.sh
+++ ROSwifi.sh
@@ -1,3 +1,3 @@
-export ROS_MASTER_URI=http://robotics-HP-EliteBook-8440p:11311
+export ROS_MASTER_URI=http://robotics-Latitude-E6410:11311
 export ROS_IP=172.31.99.88
 roslaunch lunabotics2020 Joy.launch
